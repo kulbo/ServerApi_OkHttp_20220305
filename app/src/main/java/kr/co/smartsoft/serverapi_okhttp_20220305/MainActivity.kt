c6310c4b5d131f3b4dfd15606afa5c4022467db1
이 성 노 <kulbo0926@gmail.com>
2022-03-05 01:11:09 +0900

    서버와의 통신 http 서버 활용

---
+++ app/src/main/java/kr/co/smartsoft/serverapi_okhttp_20220305/MainActivity.kt
@@ -0,0 +1,11 @@
+package kr.co.smartsoft.serverapi_okhttp_20220305
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
