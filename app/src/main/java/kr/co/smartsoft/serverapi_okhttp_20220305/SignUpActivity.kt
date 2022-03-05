87fbd410c54ccd944a9000d0b6d0e517c3345ed3
이 성 노 <kulbo0926@gmail.com>
2022-03-06 06:37:33 +0900

    가입 성공시, 그 사람의 닉네임까지 파싱해서 토스트 문구로 사용

--- app/src/main/java/kr/co/smartsoft/serverapi_okhttp_20220305/SignUpActivity.kt
+++ app/src/main/java/kr/co/smartsoft/serverapi_okhttp_20220305/SignUpActivity.kt
@@ -2,6 +2,7 @@ package kr.co.smartsoft.serverapi_okhttp_20220305
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import kr.co.smartsoft.serverapi_okhttp_20220305.databinding.ActivitySignUpBinding
 import kr.co.smartsoft.serverapi_okhttp_20220305.utils.ServerUtil
@@ -26,6 +27,25 @@ class SignUpActivity : BaseActivity() {
 
             ServerUtil.putRequestSignUp(inputEmail, inputPw, inputNickname, object : ServerUtil.JsonResponseHandler{
                 override fun onResponse(jsonObject: JSONObject) {
+//                  회원가입 성공/실패 분기
+                    val code = jsonObject.getInt("code")
+                    if (code == 200) {
+                        val dataObj = jsonObject.getJSONObject("data")
+                        val userObj = dataObj.getJSONObject("user")
+                        val nickname = userObj.getString("nick_name")
+
+                        runOnUiThread {
+                            Toast.makeText(mContext, "${nickname}님, 가입을 축하합니다.", Toast.LENGTH_SHORT).show()
+                        }
+                        finish()
+                    }
+                    else {
+                        val message = jsonObject.getString("message")
+                        runOnUiThread {
+                            Toast.makeText(mContext, "실패 사유 : ${message}", Toast.LENGTH_SHORT).show()
+                        }
+
+                    }
 
                 }
 
