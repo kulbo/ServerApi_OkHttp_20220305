fed216a4b3e02604c68d2d9cc4192b7c4dd83ff7
이 성 노 <kulbo0926@gmail.com>
2022-03-05 22:24:01 +0900

    코틀린단에서도 바인딩 변술 설정

--- app/src/main/java/kr/co/smartsoft/serverapi_okhttp_20220305/SignUpActivity.kt
+++ app/src/main/java/kr/co/smartsoft/serverapi_okhttp_20220305/SignUpActivity.kt
@@ -2,10 +2,15 @@ package kr.co.smartsoft.serverapi_okhttp_20220305
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import kr.co.smartsoft.serverapi_okhttp_20220305.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+
+    lateinit var binding:ActivitySignUpBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_sign_up)
     }
 }
