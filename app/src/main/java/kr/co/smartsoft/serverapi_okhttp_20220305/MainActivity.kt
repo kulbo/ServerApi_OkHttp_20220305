0a97c9df58ff83d89e97c26c7f1c1448fcabf962
이 성 노 <kulbo0926@gmail.com>
2022-03-05 22:15:50 +0900

    로그인 실패시, 서버가 주는 실패사유를 String으로 받아서, 토스터로 출력  - 서버의 데이터를 > UI에 활용

--- app/src/main/java/kr/co/smartsoft/serverapi_okhttp_20220305/MainActivity.kt
+++ app/src/main/java/kr/co/smartsoft/serverapi_okhttp_20220305/MainActivity.kt
@@ -2,9 +2,11 @@ package kr.co.smartsoft.serverapi_okhttp_20220305
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import kr.co.smartsoft.serverapi_okhttp_20220305.databinding.ActivityMainBinding
 import kr.co.smartsoft.serverapi_okhttp_20220305.utils.ServerUtil
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,7 +25,27 @@ class MainActivity : AppCompatActivity() {
             val inputId = binding.edtId.text.toString()
             val inputPw = binding.edtPassword.text.toString()
 
-            ServerUtil.postRequestLogin(inputId, inputPw)
+            ServerUtil.postRequestLogin(inputId, inputPw, object : ServerUtil.JsonResponseHandler{
+                override fun onResponse(jsonObject: JSONObject) {
+//                    화면의 입장에서 로그인 결과를 받아서 처리할 코드
+//                    서버에 다녀오고 실행 : 라이브러러가 자동으로 백구라운드에서 만들어 둔 코드.
+
+                    val code = jsonObject.getInt("code")
+                    if (code == 200) {
+                        runOnUiThread {
+                            Toast.makeText(this@MainActivity, "로드인 성공", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                    else {
+                        val message = jsonObject.getString("message")
+//                        토스트 : UI조작. => 백그라운드에서 UI를 건드리면 위험한동작으로 간주하고 앱을 강제 종료
+                        runOnUiThread {
+                            Toast.makeText(this@MainActivity, message, Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+
+            })
         }
     }
 
