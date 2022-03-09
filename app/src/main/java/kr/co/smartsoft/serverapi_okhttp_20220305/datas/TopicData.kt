cea18d7344ee0d85e017bf4b78664a142af6fefb
이 성 노 <kulbo0926@gmail.com>
2022-03-09 17:04:33 +0900

    토론 주제 표현용 데이터 클래스 추가  - 생성자 x, 멤버변수만 추가  - 서버의 데이터를 참고해서 추가

--- app/src/main/java/kr/co/smartsoft/serverapi_okhttp_20220305/datas/TopicData.kt
+++ app/src/main/java/kr/co/smartsoft/serverapi_okhttp_20220305/datas/TopicData.kt
@@ -3,8 +3,10 @@ package kr.co.smartsoft.serverapi_okhttp_20220305.datas
 import java.io.Serializable
 
 class TopicData: Serializable {
-    var id = 0
-    var title = ""
-//    var
+    var id = 0          //id는 Int라고 명시
+    var title = ""      //title은 String이라고 명시
+    var imageURL = ""   //서버 : img_url, 앱 : imageURL 변수명 다른경우
+    var replyCount = 0
+
 
 }
