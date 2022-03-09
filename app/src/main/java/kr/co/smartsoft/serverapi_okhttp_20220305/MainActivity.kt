fb2645ee29163272785061569bd81d1613430e68
이 성 노 <kulbo0926@gmail.com>
2022-03-09 11:03:19 +0900

    메인화명 추가, 데이터바인딩 세팅

---
+++ app/src/main/java/kr/co/smartsoft/serverapi_okhttp_20220305/MainActivity.kt
@@ -0,0 +1,15 @@
+package kr.co.smartsoft.serverapi_okhttp_20220305
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import kr.co.smartsoft.serverapi_okhttp_20220305.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+}
