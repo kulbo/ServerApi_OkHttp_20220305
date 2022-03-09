3ed2234da2a33c8a0cae351068e3a16fab37914d
이 성 노 <kulbo0926@gmail.com>
2022-03-09 19:06:35 +0900

    로딩화면 추가

---
+++ app/src/main/java/kr/co/smartsoft/serverapi_okhttp_20220305/SplashActivity.kt
@@ -0,0 +1,21 @@
+package kr.co.smartsoft.serverapi_okhttp_20220305
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        setupEvents()
+        setValues()
+    }
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
