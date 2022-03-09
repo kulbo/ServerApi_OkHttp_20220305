b5e61542c5d9dcf7f9a85951fc90129f6b84d172
이 성 노 <kulbo0926@gmail.com>
2022-03-09 11:05:32 +0900

    BaseActivty 상속 처리  - 모든 액티비티에 이 세팅도 진행 예정

--- app/src/main/java/kr/co/smartsoft/serverapi_okhttp_20220305/MainActivity.kt
+++ app/src/main/java/kr/co/smartsoft/serverapi_okhttp_20220305/MainActivity.kt
@@ -5,11 +5,22 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import kr.co.smartsoft.serverapi_okhttp_20220305.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
     lateinit var binding:ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setupEvents()
+        setValues()
     }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
 }
