7486552b1c542e3b5808bc6339edf77bf6574b09
이 성 노 <kulbo0926@gmail.com>
2022-03-10 22:20:41 +0900

    득표 현황 새로고침  - 미리 구조를 잡아둔 함수 활용

--- app/src/main/java/kr/co/smartsoft/serverapi_okhttp_20220305/ViewTopicDetailActivity.kt
+++ app/src/main/java/kr/co/smartsoft/serverapi_okhttp_20220305/ViewTopicDetailActivity.kt
@@ -2,6 +2,7 @@ package kr.co.smartsoft.serverapi_okhttp_20220305
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.bumptech.glide.Glide
 import kr.co.smartsoft.serverapi_okhttp_20220305.databinding.ActivityViewTopicDetailBinding
@@ -29,10 +30,16 @@ class ViewTopicDetailActivity : BaseActivity() {
         binding.btnVote1.setOnClickListener {
 //            서버의 투표 API 호출
             ServerUtil.postRequestVote(mContext, mTopicData.sideList[0].id, object : ServerUtil.JsonResponseHandler{
-                override fun onResponse(jsonObject: JSONObject) {
+                override fun onResponse(jsonObj: JSONObject) {
 
+//                    토스트로 서버가 알려준 현재 상황(신규 투표 or 재투표 or 취소 등)
+                    val message = jsonObj.getString("message")
 
+                    runOnUiThread {
+                        Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+                    }
 
+                    getTopicDetailFromServer()
                 }
 
             })
