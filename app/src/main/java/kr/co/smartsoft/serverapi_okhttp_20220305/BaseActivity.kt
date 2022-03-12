6ed27a8a361e287ea5e1bf42ce669af6a6d4c23b
이 성 노 <kulbo0926@gmail.com>
2022-03-12 20:05:43 +0900

    액션바의 UI를 BaseActivity 에서 끌어와서 이벤트 처리

--- app/src/main/java/kr/co/smartsoft/serverapi_okhttp_20220305/BaseActivity.kt
+++ app/src/main/java/kr/co/smartsoft/serverapi_okhttp_20220305/BaseActivity.kt
@@ -2,6 +2,7 @@ package kr.co.smartsoft.serverapi_okhttp_20220305
 
 import android.content.Context
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
@@ -10,6 +11,10 @@ abstract class BaseActivity : AppCompatActivity() {
 //    Context 계열의 파라미터에 대입할때, 보통 this 로 대입
 //      인터페이스가 엮이기 시작하면 this@어느화면 추가로 고려
     lateinit var mContext:Context
+
+//    액션바의 UI 변수를 멤버변수로 > 상속 가능
+//    => 변수에 대입 : 커스텀 액션바 세팅 뒤에
+    lateinit var btnBack: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,6 +43,14 @@ abstract class BaseActivity : AppCompatActivity() {
 
         val toolbar = defaultActionBar.customView.parent as Toolbar
         toolbar.setContentInsetsAbsolute(0,0)
+
+//        xml에 그려둔 UI 가져오기
+        btnBack = defaultActionBar.customView.findViewById(R.id.btnBack)
+
+//      누르면 화면 종료 : 모든 화면 공통
+        btnBack.setOnClickListener {
+            finish()
+        }
 
     }
 }
