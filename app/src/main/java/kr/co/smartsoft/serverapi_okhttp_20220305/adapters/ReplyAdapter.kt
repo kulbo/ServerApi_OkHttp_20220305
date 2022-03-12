a15d4c8c7e04833fdf49f05eab809810a8ddcbb3
이 성 노 <kulbo0926@gmail.com>
2022-03-12 22:01:13 +0900

    내 좋아요 여부에 따른 배경 drawable 변경

--- app/src/main/java/kr/co/smartsoft/serverapi_okhttp_20220305/adapters/ReplyAdapter.kt
+++ app/src/main/java/kr/co/smartsoft/serverapi_okhttp_20220305/adapters/ReplyAdapter.kt
@@ -72,9 +72,11 @@ class ReplyAdapter(
 //        좋아요가 눌렸는지, 아닌지, 글씨 색상 변경.
         if (data.isMyLike) {
             txtLikeCount.setTextColor(ContextCompat.getColor(mContext, R.color.naver_red))
+            txtLikeCount.setBackgroundResource(R.drawable.naver_red_border_box)
         }
         else {
             txtLikeCount.setTextColor(ContextCompat.getColor(mContext, R.color.deep_dark_gray))
+            txtLikeCount.setBackgroundResource(R.drawable.dark_gray_border_box)
         }
         return row
     }
